9f5e1ce6a57473948bd98be0f1e3e22dbf130a13
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-04-15 19:08:45 +0800

    Add test fixtures

--- src/test/java/seedu/address/testutil/TypicalProducts.java
+++ src/test/java/seedu/address/testutil/TypicalProducts.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.money.Money;
 import seedu.address.model.product.Category;
 import seedu.address.model.product.Product;
@@ -19,29 +20,38 @@ public class TypicalProducts {
     //Prevent instantiation
     private TypicalProducts() {};
 
-    //Product IDs are set manually for first few to ensure that IDs 1-6 are present, then automatically set later
-    public static final Product EGG = new Product(1, new ProductName("Egg"),
-            new Money(new BigDecimal(0.5)), new Category("Food"));
-    public static final Product MILK = new Product(2, new ProductName("Milk"),
-            new Money(new BigDecimal(2.0)), new Category("Food"));
-    public static final Product SHIRT = new Product(3, new ProductName("Shirt"),
-            new Money(new BigDecimal(15.0)), new Category("Fashion"));
-    public static final Product PANTS = new Product(4, new ProductName("Pants"),
-            new Money(new BigDecimal(1.0)), new Category("Fashion"));
-    public static final Product LAPTOP = new Product(5, new ProductName("Laptop"),
-            new Money(new BigDecimal(2000)), new Category("Tech"));
-    public static final Product SWEET = new Product(6, new ProductName("Sweet"),
-            new Money(new BigDecimal(0.05)), new Category("Food"));
-    public static final Product PAPER = new Product(new ProductName("Paper"),
-            new Money(new BigDecimal(5)), new Category("Stationery"));
-    public static final Product PEN = new Product(new ProductName("Pen"),
-            new Money(new BigDecimal(1.5)), new Category("Stationery"));
-    public static final Product PENCIL = new Product(new ProductName("Pencil"),
-            new Money(new BigDecimal(1.0)), new Category("Stationery"));
-    public static final Product SODA = new Product(new ProductName("Soda"),
-            new Money(new BigDecimal(2.0)), new Category("Food"));
+    public static Product EGG, MILK, SHIRT, PANTS, LAPTOP, SWEET, PAPER, PEN, PENCIL, SODA;
 
-    public static List<Product> getTypicalOrders() {
+    //Wrap in static block because Money.parsePrice() throws IllegalValueException
+    static {
+        try {
+            //Product IDs are set manually for first few to ensure that IDs 1-6 are present, then automatically set later
+            EGG = new Product(1, new ProductName("Egg"),
+                    Money.parsePrice("0.5"), new Category("Food"));
+            MILK = new Product(2, new ProductName("Milk"),
+                    Money.parsePrice("2"), new Category("Food"));
+            SHIRT = new Product(3, new ProductName("Shirt"),
+                    Money.parsePrice("15"), new Category("Fashion"));
+            PANTS = new Product(4, new ProductName("Pants"),
+                    Money.parsePrice("20"), new Category("Fashion"));
+            LAPTOP = new Product(5, new ProductName("Laptop"),
+                    Money.parsePrice("2000"), new Category("Tech"));
+            SWEET = new Product(6, new ProductName("Sweet"),
+                    Money.parsePrice("0.05"), new Category("Food"));
+            PAPER = new Product(new ProductName("Paper"),
+                    Money.parsePrice("5"), new Category("Stationery"));
+            PEN = new Product(new ProductName("Pen"),
+                    Money.parsePrice("1.5"), new Category("Stationery"));
+            PENCIL = new Product(new ProductName("Pencil"),
+                    Money.parsePrice("1"), new Category("Stationery"));
+            SODA = new Product(new ProductName("Soda"),
+                    Money.parsePrice("2"), new Category("Food"));
+        } catch (IllegalValueException ive) {
+            ive.printStackTrace();
+        }
+    }
+
+    public static List<Product> getTypicalProducts() {
         return new ArrayList<Product>(Arrays.asList(EGG, MILK, SHIRT, PANTS, LAPTOP, SWEET, PAPER, PEN, PENCIL, SODA));
     }
 
