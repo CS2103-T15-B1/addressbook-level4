9a111487dab6aadbde53c5e4e5e37156365e2bb9
lowjiajin <pixelrife@hotmail.com>
2018-04-15 12:41:21 +0800

    Added comments to all code

--- src/main/java/seedu/address/logic/recommender/BuyDecision.java
+++ src/main/java/seedu/address/logic/recommender/BuyDecision.java
@@ -3,9 +3,10 @@ package seedu.address.logic.recommender;
 import static java.lang.Double.compare;
 
 //@@author lowjiajin
+
 /**
  * Represents the confidence in the decision of whether to buy a given product, referenced by its {@code productName}.
- * Package private to Recommender
+ * Package private to Recommender.
  */
 class BuyDecision implements Comparable<BuyDecision> {
     private String productName;
@@ -25,13 +26,18 @@ class BuyDecision implements Comparable<BuyDecision> {
     }
 
     /**
-     * Used in sorting the recommendations so only the most confident recommendations are presented.
+     * Used in sorting the recommendations by probability of purchase,
+     * so the most confident recommendations are presented first.
      */
     @Override
     public int compareTo(BuyDecision other) {
         return compare(other.getBuyProb(), buyProb);
     }
 
+    /**
+     * Controls how the each product's recommendation is displayed in the CLI output.
+     * {@code productName} and {@code buyProb} delimited with a colon and space.
+     */
     @Override
     public String toString() {
         return String.format("%1$s: %2$f", productName, buyProb);
