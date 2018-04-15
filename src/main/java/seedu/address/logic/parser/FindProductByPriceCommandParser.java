6aaf51bde13554e50ea6e1a62319e9fe592e64e8
lowjiajin <pixelrife@hotmail.com>
2018-04-15 12:47:38 +0800

    Added comments to aggregate getters and their parsers

--- src/main/java/seedu/address/logic/parser/FindProductByPriceCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindProductByPriceCommandParser.java
@@ -13,13 +13,23 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_MIN_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MAX_PRICE;
 
 //@@author lowjiajin
+
+/**
+ * Parses input arguments and creates a new FindProductByPriceCommand object
+ */
 public class FindProductByPriceCommandParser implements Parser<FindProductByPriceCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments for the min and max price delimiters of the
+     * FindProductByPriceCommand and returns a FindProductByPriceCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
     public FindProductByPriceCommand parse(String args) throws ParseException {
 
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_MIN_PRICE, PREFIX_MAX_PRICE);
 
+        // Ensures that the input conforms to the min and max price argument delimiters.
         if (!arePrefixesPresent(argMultimap, PREFIX_MIN_PRICE, PREFIX_MAX_PRICE)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindProductByPriceCommand.MESSAGE_USAGE));
