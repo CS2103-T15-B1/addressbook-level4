3374c12b90dd2b4fdb0738d65f839ed252bf6413
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-04-15 16:03:27 +0800

    Minor fixes

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -115,4 +115,90 @@ public class AddressBookParser {
         }
     }
 
+    //@@author qinghao1
+    /**
+     * Overload parseCommand for commands that do not need addressBook, to maintain backwards compatibility
+     * @param userInput
+     * @return
+     * @throws ParseException
+     */
+    public Command parseCommand(String userInput) throws ParseException {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+        switch (commandWord) {
+
+            case AddCommand.COMMAND_WORD:
+                return new AddCommandParser().parse(arguments);
+
+            case AddProductCommand.COMMAND_WORD:
+                return new AddProductCommandParser().parse(arguments);
+
+            case EditCommand.COMMAND_WORD:
+                return new EditCommandParser().parse(arguments);
+
+            case SelectCommand.COMMAND_WORD:
+                return new SelectCommandParser().parse(arguments);
+
+            case MapCommand.COMMAND_WORD:
+                return new MapCommandParser().parse(arguments);
+
+            case DeleteCommand.COMMAND_WORD:
+                return new DeleteCommandParser().parse(arguments);
+
+            case DeleteProductCommand.COMMAND_WORD:
+                return new DeleteProductCommandParser().parse(arguments);
+
+            case DeleteOrderCommand.COMMAND_WORD:
+                return new DeleteOrderCommandParser().parse(arguments);
+
+            case ClearCommand.COMMAND_WORD:
+                return new ClearCommand();
+
+            case FindCommand.COMMAND_WORD:
+                return new FindCommandParser().parse(arguments);
+
+            //@@author lowjiajin
+            case FindProductByCategoryCommand.COMMAND_WORD:
+                return new FindProductByCategoryCommandParser().parse(arguments);
+
+            case FindProductByNameCommand.COMMAND_WORD:
+                return new FindProductByNameCommandParser().parse(arguments);
+
+            case FindProductByPriceCommand.COMMAND_WORD:
+                return new FindProductByPriceCommandParser().parse(arguments);
+
+            case ListProductCommand.COMMAND_WORD:
+                return new ListProductCommand();
+
+            //@@author
+            case ListCommand.COMMAND_WORD:
+                return new ListCommand();
+
+            case HistoryCommand.COMMAND_WORD:
+                return new HistoryCommand();
+
+            case ExitCommand.COMMAND_WORD:
+                return new ExitCommand();
+
+            case HelpCommand.COMMAND_WORD:
+                return new HelpCommand();
+
+            case UndoCommand.COMMAND_WORD:
+                return new UndoCommand();
+
+            case RedoCommand.COMMAND_WORD:
+                return new RedoCommand();
+
+            case AddOrderCommand.COMMAND_WORD:
+                return new AddOrderCommandParser().parse(arguments);
+
+            default:
+                throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        }
+    }
 }
