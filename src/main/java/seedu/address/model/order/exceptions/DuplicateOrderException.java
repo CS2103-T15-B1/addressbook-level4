533a0eff17acd710519ce0cd58b9b7951262a503
Low Jia Jin <pixelrife@hotmail.com>
2018-04-15 19:00:46 +0800

    Added more collation tags

--- src/main/java/seedu/address/model/order/exceptions/DuplicateOrderException.java
+++ src/main/java/seedu/address/model/order/exceptions/DuplicateOrderException.java
@@ -2,6 +2,11 @@ package seedu.address.model.order.exceptions;
 
 import seedu.address.commons.exceptions.DuplicateDataException;
 
+//@@author lowjiajin
+
+/**
+ * Signals that the operation will result in duplicate Order objects.
+ */
 public class DuplicateOrderException extends DuplicateDataException {
     public DuplicateOrderException() {
         super("Operation would result in duplicate orders");
